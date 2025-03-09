5bdb9576e4c588997fe0dace93fa962868d8485d
mustafa-akgul <akgulmustafatalha79@gmail.com>
2025-03-09 13:33:01 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,117 @@
 import 'package:flutter/material.dart';
 import 'package:smartedu/OnboardingScreen.dart';
-import 'package:smartedu/SWelcomeScreen.dart';
-import 'package:smartedu/TWelcomeScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: OnboardingScreen()
+      home: const MainScreen(),
     );
   }
 }
 
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Ana ekran yüklendikten sonra 3 saniye bekleyip Onboarding ekranına geçiş yapacak
+    Future.delayed(const Duration(seconds: 6), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => OnboardingScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Ekran(),
+        ],
+      ),
+    );
+  }
+}
+
+// İlk ekran widget'ı
+class Ekran extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 913,
+          padding: const EdgeInsets.only(
+            top: 217,
+            left: 38.80,
+            right: 38.80,
+            bottom: 204.09,
+          ),
+          clipBehavior: Clip.none,
+          decoration: ShapeDecoration(
+            color: const Color(0xFFD9FBFF),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18),
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 381.39,
+                height: 381.39,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/logo.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 19.61),
+              Container(
+                width: 225.07,
+                height: 90.91,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/loading.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
