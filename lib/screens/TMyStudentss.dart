310203f76bf4a94cc7f816f03a1d30b5da35448e
zehra <ballizehra2007@gmail.com>
2025-04-09 00:14:56 +0300

    Added TMyStudentss.dart file

---
+++ lib/screens/TMyStudentss.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TMyStudentss extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Öğrencilerim'),
+        backgroundColor: Color(0xFF50D4DB),
+      ),
+      body: Center(
+        child: Text(
+          'Bu sayfa henüz içerik eklenmedi.',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
