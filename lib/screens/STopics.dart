2e96906ab1ba88e4fff03f9bcff42137576f7c52
zehra <ballizehra2007@gmail.com>
2025-05-14 21:03:03 +0300

    STopics.dart eklendi

---
+++ lib/screens/STopics.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class STopics extends StatelessWidget {
+  final String lessonTitle;
+
+  const STopics({super.key, required this.lessonTitle});
+
+  final List<String> topics = const [
+    'Doğal Sayılar',
+    'Toplama ve Çıkarma İşlemi',
+    'Çarpma ve Bölme İşlemi',
+    'Kesirler',
+    'Geometrik Şekiller',
+    'Alan Hesaplama',
+    'Zaman Ölçme',
+    'Paralarımız',
+    'Tartma',
+    'Geometrik Cisimler Ve Şekiller',
+    'Geometrik Örüntüler',
+    'Geometrik Temel Kavramlar',
+    'Uzamsal İlişkiler',
+    'Uzunluk - Çevre - Alan Ölçme',
+    'Sıvı Ölçme',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          lessonTitle,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF2C3E50),
+          ),
+        ),
+        backgroundColor: const Color(0xFFFF5C5C).withOpacity(0.8),
+        iconTheme: const IconThemeData(color: Colors.black),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      backgroundColor: const Color(0xFFF5F3FF),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        child: Column(
+          children: topics.map((topic) {
+            return Container(
+              margin: const EdgeInsets.symmetric(vertical: 6),  // Butonlar arasındaki boşluk
+              padding: const EdgeInsets.all(16),
+              width: double.infinity,  // Butonların aynı genişlikte olmasını sağlar
+              height: 80,  // Yüksekliği artırdık
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.redAccent),
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Center(  // Başlıkların ortalanması için Center widget'ı ekledik
+                child: Text(
+                  topic,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,  // Yazı boyutunu artırdık
+                    color: Colors.black,  // Yazı rengini siyah yaptık
+                  ),
+                ),
+              ),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
+}
