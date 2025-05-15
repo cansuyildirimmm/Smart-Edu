50c5b40f5b2c21ea0bc1218d20b0b8352bbc252c
Mustafa Talha Akgül <152786203+mustafa-akgul@users.noreply.github.com>
2025-05-15 10:18:25 +0300

    Update main.dart
    Büşra'nın main.dart ==> Zehra'nın main.dart'a kopyalandı(çakışmayı önlemek adına)

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,109 @@
+
 import 'package:flutter/material.dart';
-//import 'package:smartedu/screens/SHome.dart';
-//import 'screens/TMainMenuScreen.dart';
-import 'screens/SMainMenuScreen.dart'; 
-//import 'screens/SMyLessons.dart'; 
+import 'OnboardingScreen.dart';  // Onboarding ekranını içeri aktardık
 
 void main() {
-  runApp(MyApp());
+  runApp(const FigmaToCodeApp());
 }
 
-class MyApp extends StatelessWidget {
+class FigmaToCodeApp extends StatelessWidget {
+  const FigmaToCodeApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      //home: TeacherMenuScreen(), 
-      home: SMainMenuScreen(),
-      //home: SHome(),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
+      ),
+      home: const MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Ana ekran yüklendikten sonra 3 saniye bekleyip Onboarding ekranına geçicek
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => OnboardingScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Ekran(),
+        ],
+      ),
+    );
+  }
+}
+
+// İlk ekran widget'ı
+class Ekran extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 913,
+          padding: const EdgeInsets.only(
+            top: 217,
+            left: 38.80,
+            right: 38.80,
+            bottom: 204.09,
+          ),
+          clipBehavior: Clip.none,
+          decoration: ShapeDecoration(
+            color: const Color(0xFFD9FBFF),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18),
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: 381.39,
+                height: 381.39,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/logo.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 19.61),
+              Container(
+                width: 225.07,
+                height: 90.91,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/images/loading.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
